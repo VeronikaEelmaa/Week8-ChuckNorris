4dc04d7510df00167eaa233d136941d6f8713b53
Veronika Eelmaa <veronika.eelmaa@gmail.com>
2022-06-06 02:26:10 +0300

    DirectoriesAndFiles

---
+++ ShoppingFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\...\Samples";
+            Console.WriteLine("Lets create you a shopping list and lets call it 'shoppingList'. Please enter folder name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Call your list 'myShoppingList.txt'. Enter file name:");
+            string fileName = Console.ReadLine();
+
+            Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+            File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+        }
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                
+                 
+            string fileLocation = @"C:\Users\...\Samples\shoppingList";
+            string fileName = @"\\myShoppingList.txt";
+            
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to write a shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your wish:");
+                    string userWish = Console.ReadLine();
+                    myShoppingList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Ok, lets do it on another time! Take care!");
+                }
+
+            }
+
+            foreach (string wish in myShoppingList)
+            {
+                Console.WriteLine($"You would like to buy: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+
+
+        }
+    }
+}
